075aadf705fa24bc41fd04419b23769644908d05
upi1682 <upi538287@gmail.com>
2025-06-26 18:41:13 +0700

    jadikan anak kanan titik induk ke simpul baru

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -83,7 +83,8 @@ public:
         // step 7 : if the value in the data field of the new node is greater than that of the new node
         else if (x > parent->info)
         {
-          
+            // 7a : make the right chid of parent point to the new node
+            parent->rightchild = newNode;
 
         }
     }
